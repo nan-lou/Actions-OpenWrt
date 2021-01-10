352394f3a7dfec0fc43dfaa3f53633a1ee71cd51
piaoyizy <925754@qq.com>
2021-01-10 09:15:38 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,4 +11,22 @@
 #
 
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/10.10.10.6/g' package/base-files/files/bin/config_generate
+
+
+
+ 
+
+# theme: atmaterial
+git clone https://github.com/sypopo/luci-theme-atmaterial.git theme-temp/luci-theme-atmaterial
+rm -rf theme-temp/luci-theme-atmaterial/LICENSE
+rm -rf theme-temp/luci-theme-atmaterial/README.md
+rm -rf package/lean/luci-theme-atmaterial
+mv -f theme-temp/luci-theme-atmaterial package/lean/
+cat >> .config <<EOF
+CONFIG_PACKAGE_luci-theme-atmaterial=y
+EOF
+#rm -rf theme-temp
+
+default_theme='atmaterial'
+sed -i "s/bootstrap/$default_theme/g" feeds/luci/modules/luci-base/root/etc/config/luci
