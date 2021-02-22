c38b1bea5bfbe52f8110b977a68681913945c1e9
piaoyizy <925754@qq.com>
2021-02-22 12:34:47 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -21,8 +21,6 @@ rm -rf package/lean/luci-theme-argon
 #git clone https://github.com/jerrykuku/luci-theme-argon -b 19.07_stable package/lean/luci-theme-argon
 git clone -b master https://github.com/jerrykuku/luci-theme-argon package/lean/luci-theme-argon
 
-
-
-
+#'设置默认主题'
 default_theme='argon'
 sed -i "s/bootstrap/$default_theme/g" feeds/luci/modules/luci-base/root/etc/config/luci
