7ebb85c841dde3c7fdf04107eadb9dcf7b18602a
piaoyizy <925754@qq.com>
2021-02-23 16:40:42 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -19,8 +19,8 @@ rm -rf package/lean/luci-theme-argon
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
 
 #'godproxy'
-rm -rf package/lean/luci-theme-argon
-git clone https://github.com/godros/luci-app-godproxy.git package/lean/luci-theme-argon
+rm -rf package/lean/luci-app-godproxy
+git clone https://github.com/godros/luci-app-godproxy.git package/lean/luci-app-godproxy
 
 # theme: atmaterial	
 rm -rf package/lean/luci-theme-atmaterial	
