c5cf098e4264a71a49cf8b4d4ecef5323eb267ee
piaoyizy <925754@qq.com>
2021-02-25 19:32:33 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -28,9 +28,7 @@ git clone https://github.com/sypopo/luci-theme-atmaterial.git  package/lean/luci
 
 cat >> .config <<EOF	
 CONFIG_PACKAGE_luci-theme-atmaterial=y	
-CONFIG_PACKAGE_luci-app-godproxy=y	
 CONFIG_PACKAGE_luci-theme-edge=y	
-
 EOF	
 
 
