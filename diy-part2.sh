d3a94a064b8ae4b64573ae7c2e982aa2125fbe27
nan-lou <726758103@qq.com>
2021-03-08 17:21:36 +0800

    -

--- diy-part2.sh
+++ diy-part2.sh
@@ -16,20 +16,20 @@ sed -i 's/192.168.1.1/10.10.10.6/g' package/base-files/files/bin/config_generate
  
 #'删除argon主题，并拉最新版'
 rm -rf package/lean/luci-theme-edge
-git clone -b 18.06 https://github.com/garypang13/luci-theme-edge.git package/lean/luci-theme-edge
+git clone -b 18.06 --depth=1 https://github.com/garypang13/luci-theme-edge.git package/lean/luci-theme-edge
 
 #'godproxy'
 rm -rf package/lean/luci-app-godproxy
-git clone https://github.com/godros/luci-app-godproxy.git package/lean/luci-app-godproxy
+git clone --depth=1 https://github.com/godros/luci-app-godproxy.git package/lean/luci-app-godproxy
 
 # theme: atmaterial	
 rm -rf package/lean/luci-theme-atmaterial	
-git clone https://github.com/sypopo/luci-theme-atmaterial.git  package/lean/luci-theme-atmaterial	
+git clone --depth=1 https://github.com/sypopo/luci-theme-atmaterial.git  package/lean/luci-theme-atmaterial	
 
-cat >> .config <<EOF	
-CONFIG_PACKAGE_luci-theme-atmaterial=y	
-CONFIG_PACKAGE_luci-theme-edge=y	
-EOF	
+#cat >> .config <<EOF	
+#CONFIG_PACKAGE_luci-theme-atmaterial=y	
+#CONFIG_PACKAGE_luci-theme-edge=y	
+#EOF	
 
 
 #'设置默认主题'
